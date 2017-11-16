f56247d7049e22e5a935aa501b380f4395a09f77
Sergio Fernández <sergio.fernandezj@gmail.com>
2017-11-16 01:11:42 +0100

    Added day 3. CSS variables

---
+++ 03 - CSS Variables/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", event => {
+    const inputs = document.querySelectorAll(".controls input");
+
+    function handleUpdate(event) {
+        const suffix = this.dataset.sizing || "";
+        document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    }
+
+    inputs.forEach(input => input.addEventListener("change", handleUpdate));
+    inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+})
