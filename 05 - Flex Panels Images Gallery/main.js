d05e7a3b70a7d7b1ffc28e91574adfaddd4ba39b
Sergio Fernández <sergio.fernandezj@gmail.com>
2017-11-19 23:57:07 +0100

    Added day 5. Flex gallery

---
+++ 05 - Flex Panels Images Gallery/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", e => {
+    const panels = document.querySelectorAll(".panel");
+
+    function toggleOpen() {
+        this.classList.toggle("open");
+    }
+
+    function toggleActive(e) {
+        if (e.propertyName.includes("flex"))
+            this.classList.toggle("open-active");
+    }
+
+    panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+    panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
+})
